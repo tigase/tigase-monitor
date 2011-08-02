22bfd31019c4c441a1ef12e4fc89e5104bd34da5
Artur Hefczyc <kobit@tigase.org>
2011-08-02 11:53:52 +0000

    Added more configuration examples
    git-svn-id: file:///home/svn/repos/tigase-monitor/trunk@64 f3d9453a-065d-4d14-b440-5a2b23bfa02b

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -3,6 +3,6 @@
 DYLD_LIBRARY_PATH="/Users/kobit/Downloads/yjp-8.0.15/bin/mac"
 #PROFILER="-agentlib:yjpagent"
 GC="-XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:ParallelCMSThreads=2"
-JAVA_OPTIONS="${GC} ${PROFILER} -d64 -Xms100M -Xmx1500m -Dcom.apple.macos.useScreenMenuBar=true"
+JAVA_OPTIONS="${GC} ${PROFILER} -d64 -Xms100M -Xmx3000m -Dcom.apple.macos.useScreenMenuBar=true"
 CP=`ls -d libs/*.jar 2>/dev/null | tr '\n' :`
 java -Xdock:name='Tigase Monitor' ${JAVA_OPTIONS} -cp tigase-monitor.jar:libs/'*' tigase.monitor.MonitorMain --init etc/monitor.properties
