3c4ae0b531f587505d7149c704f74eadd9bd6fcc
Artur Hefczyc <kobit@tigase.org>
2012-04-30 20:28:07 +0000

    Minor updates and changes for the location for some files
    git-svn-id: file:///home/svn/repos/tigase-monitor/trunk@73 f3d9453a-065d-4d14-b440-5a2b23bfa02b

--- bin/prepare-dist.sh
+++ bin/prepare-dist.sh
@@ -6,11 +6,11 @@ if [ "$1" == "" ] ; then
 fi
 
 DATE=`date +"%Y%m%d"`
-DIR="tigase-monitor_$1_$DATE"
+DIR="dists/tigase-monitor_$1_$DATE"
 
 rm -rf $DIR $DIR.tgz
 
-mkdir $DIR
+mkdir -p $DIR
 mkdir $DIR/etc
 mkdir $DIR/libs
 mkdir $DIR/sounds
@@ -20,6 +20,7 @@ mkdir $DIR/sounds
 cp target/tigase-monitor*jar-with-dependencies.jar $DIR/libs/tigase-monitor.jar
 cp sounds/* $DIR/sounds/
 cp bin/monitor.sh $DIR/
+#cp etc/default-init.properties etc/${1}-init.properties
 cp etc/${1}-init.properties $DIR/etc/monitor.properties
 cp ../tigase-private/client-licenses/${1}-monitor.licence $DIR/etc/monitor.licence
 
